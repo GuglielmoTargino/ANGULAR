c7fed68685535878a5a5662467ae61f13e4d19a5
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2025-02-04 14:52:52 -0300

    repraro
    Repaginação

--- segundo_proj_angular/src/app/componente/home/home.component.ts
+++ segundo_proj_angular/src/app/componente/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-home23',
   imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  nome="guga";
+    id=45;
 
 }
