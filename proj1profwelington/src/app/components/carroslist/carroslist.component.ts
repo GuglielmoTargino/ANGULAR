d864135245e34a8ed74dc1867cb4ace3597d7c04
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2025-04-25 15:28:54 -0300

    ok_aula10
    Finalizada a aula 10 com criação de model para manipulação com os componentes.

--- proj1profwelington/src/app/components/carroslist/carroslist.component.ts
+++ proj1profwelington/src/app/components/carroslist/carroslist.component.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import { Carros } from '../../models/carros';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-carroslist',
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './carroslist.component.html',
     styleUrl: './carroslist.component.css'})
-    export class CarroslistComponent {}
+    export class CarroslistComponent {
+
+        lista: Carros[]=[];
+
+        constructor(){
+
+            let carro: Carros= new Carros();
+            carro.id=1;
+            carro.marca="fiat";
+            carro.nome="Brava";
+
+            let carro2: Carros= new Carros();
+            carro2.id=2;
+            carro2.marca="fiat";
+            carro2.nome="Marea";
+
+            let carro3: Carros= new Carros();
+            carro3.id=3;
+            carro3.marca="fiat";
+            carro3.nome="Tempra";
+
+            this.lista.push(carro);
+            this.lista.push(carro2);
+            this.lista.push(carro3);
+        }
+    }
