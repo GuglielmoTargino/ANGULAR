967204e45eb75b271c72a5247787d186c617eb3d
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2025-05-06 19:25:06 -0300

    ok_tela_log
    Finalizada a tela de login em html.

--- prof_WO_aul_md/src/app/components/layout/login/login.component.ts
+++ prof_WO_aul_md/src/app/components/layout/login/login.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+;
 
 @Component({
   selector: 'app-login',
-  imports: [MdbFormsModule],
+  imports: [MdbFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  usu!:string;
+  sen!:string;
+
+  router =inject(Router);
+
+  logar(){
+    if(this.usu=='admin' && this.sen=='admin'){
+      this.router.navigate(['admin/carros']);
+    }else
+    alert("Senha ou usuário errado");
+  }
 
 }
