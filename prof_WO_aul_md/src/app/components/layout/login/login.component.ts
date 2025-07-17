4faa3d6b179917cb20e5e79b8c57cfa0e637601b
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2025-07-16 23:04:39 -0300

    adid
    Adição do pacote login

--- prof_WO_aul_md/src/app/components/layout/login/login.component.ts
+++ prof_WO_aul_md/src/app/components/layout/login/login.component.ts
@@ -2,6 +2,53 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 import { Router } from '@angular/router';
+import { LoginService } from '../../../auth/login.service';
+import { Login } from '../../../auth/login';
+;
+
+@Component({
+  selector: 'app-login',
+  imports: [MdbFormsModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+ login: Login = new Login();
+
+  router =inject(Router);
+  loginService = inject(LoginService);
+  constructor(){
+
+  }
+
+  logar(){
+    this.loginService.logar(this.login).subscribe({
+     next: token =>{
+
+       if (token) {
+        this.loginService.addToken(token);
+        
+      } else{
+        alert('Senha ou senha errada');
+      }
+     },
+      error: erro =>{
+        alert('Falha operação Tokrn');
+      }
+
+    });
+ 
+  }
+
+}
+
+/**
+ * 
+ * import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { Router } from '@angular/router';
+import { LoginService } from '../../../auth/login.service';
 ;
 
 @Component({
@@ -15,6 +62,7 @@ export class LoginComponent {
   sen!:string;
 
   router =inject(Router);
+  loginService = inject(LoginService)
 
   logar(){
     if(this.usu=='admin' && this.sen=='admin'){
@@ -24,3 +72,5 @@ export class LoginComponent {
   }
 
 }
+
+ */
