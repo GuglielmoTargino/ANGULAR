cad7ff47434ed597351b3a1745e52cf88ab2d9eb
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2025-09-19 03:17:48 -0300

    nd_tela_home

--- pjt8semfrt1/src/app/app.component.ts
+++ pjt8semfrt1/src/app/app.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';  // <-- ESSENCIAL
+import { CommonModule } from '@angular/common'; // <-- aqui
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule, RouterOutlet],  // <-- aqui você coloca o CommonModule
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
+
+  router = inject(Router); 
   title = 'pjt8semfrt1';
+  searchTerm: string = '';
+
+  cars = [
+    { name: 'Sedan Luxo', price: 'R$ 120.000', image: 'assets/sedan.jpg' },
+    { name: 'SUV Confort', price: 'R$ 150.000', image: 'assets/suv.jpg' },
+    { name: 'Esportivo Turbo', price: 'R$ 250.000', image: 'assets/sport.jpg' },
+  ];
+
+  get filteredCars() {
+    return this.cars.filter(car =>
+      car.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+
+login(){
+
+  this.router.navigate(['/login']); 
+  
+
+      
+  }
+
+
 }
